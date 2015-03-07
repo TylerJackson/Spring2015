bf510aa7dd84d5d8e087127f77d9f66291b56ee0
tyler jackson <tyler@apphodge.com>
2015-03-07 13:10:02 -0600

    Updating notes, and assignments for my 5 spring classes

---
+++ ArtificialIntelligence/HW2/IHw2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW.HW2
+{
+    public interface IHw2
+    {
+        Tuple<int, int> SumEvenAndOdd(List<int> input);
+
+        List<int> ListElementsDivisibleBy3(List<int> input);
+
+        List<int> ListElementsDivisibleBy3Recursive(List<int> input);
+
+        List<int> ListElementsDivisibleBy3Iterative(List<int> input);
+    }
+
+}
---
+++ ArtificialIntelligence/HW2/IHw2.fs
@@ -0,0 +1,8 @@
+﻿namespace MyListLib
+
+type IHw2 = 
+    abstract member SumEvenAndOdd : int list -> int * int
+    abstract member ListElementsDivisibleBy3 : int list -> int list
+    abstract member ListElementsDivisibleBy3Iterative : int list -> int list
+    abstract member ListElementsDivisibleBy3Recursive : int list -> int list
+
